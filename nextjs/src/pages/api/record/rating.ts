ca4eeab3f34a52d862102315bfc4077359014370
Vincent Sit <vincentsit@animaetech.com>
2022-07-24 21:18:24 +0800

    fix rating.ts

--- nextjs/src/pages/api/record/rating.ts
+++ nextjs/src/pages/api/record/rating.ts
@@ -8,9 +8,9 @@ import Cors from 'cors'
 import { runMiddleware } from 'utils/runMiddleware'
 import { getToken } from 'next-auth/jwt'
 import _ from 'lodash'
-import { Rating } from 'types'
+import { Rating, Song } from 'types'
 import Records from 'db/model/records'
-import { calculateSingleSongRating } from 'utils/calculateRating'
+import { calculateSingleSongRating, toFixedTrunc } from 'utils/calculateRating'
 import { UnauthenticatedError } from 'errors/UnauthenticatedError'
 import { getSession } from 'next-auth/react'
 // var corsOptions = {
@@ -25,16 +25,17 @@ async function handler(
 ) {
 
     if (req.method !== 'GET') throw new BadRequestError(`do not accept ${req.method} `)
-    const session = await getSession({ req})
+    const session = await getSession({ req })
     console.log("🚀 ~ file: rating.ts ~ line 29 ~ token", session
     )
-    if (!session ) throw new UnauthenticatedError('please login first')
-    
+    if (!session) throw new UnauthenticatedError('please login first')
+
     let data: any = (await Users.findOne({ where: { id: req.body.user_id }, include: { model: Records, include: [{ model: Songs }] } }))
 
     const ratingList = _.map(data.records, function (o) {
-        let rate = (Math.trunc(calculateSingleSongRating(o.song[o.difficulty], o.score) * 100) / 100).toFixed(2)
-        let result: Rating = { song: o.song.name, rating: rate, score: o.score, difficulty: o.difficulty, }
+        let song: Song = JSON.parse(o.song[o.difficulty])
+        let rating = calculateSingleSongRating(song?.rate, o.score)
+        let result: Rating = { song: o.song.display_name, combo: song?.combo, rating: rating, truncatedRating: toFixedTrunc(rating, 2), score: o.score, difficulty: o.difficulty, }
         return result
     });
     // console.log("🚀 ~ file: hello.ts ~ line 25 ~ data", data)
