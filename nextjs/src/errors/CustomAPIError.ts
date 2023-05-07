fbac4bea9884b9cf62e34df31f4ecb2582ceae45
kooki45 <kookiymleung@gmail.com>
2023-05-07 14:39:20 +0800

    update

--- nextjs/src/errors/CustomAPIError.ts
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from "http-status-codes"
-
-export class CustomAPIError extends Error {
-    declare statusCode : StatusCodes
-    constructor(message: string) {
-        super(message)
-    }
-}
-
--- nextjs/src/utils/hooks/usePrevious.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export function usePrevious<T>(value: T, defaultValue: T) {
-    const ref = useRef<T>(defaultValue);
-    useEffect(() => {
-        ref.current = value;
-    });
-    return ref.current;
-}
