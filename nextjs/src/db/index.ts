f071fd9a280bde311c2ca0244165e9ec833433ea
KooKiLeung <ming.leung@zensis.com>
2024-06-13 15:17:16 +0800

    * update db connection

--- nextjs/src/db/index.ts
+++ nextjs/src/db/index.ts
@@ -1,7 +1,21 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USERNAME as string, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: Number(process.env.DB_PORT),
-}); 
+export const sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USERNAME as string,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+        port: Number(process.env.DB_PORT),
+        ...(process.env.DB_SSL === "true"
+            ? {
+                dialectOptions: {
+                    ssl: {
+                        rejectUnauthorized: false,
+                    }
+                },
+            }
+            : ({} as any)),
+    }
+); 
